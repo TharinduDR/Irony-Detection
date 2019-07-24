e98f09c5b924985ee47dd2cd8662db10e5a8b8af
TharinduDR <rhtdranasinghe@gmail.com>
2019-07-24 04:39:12 +0100

    004: Added pre processing steps

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def clean_text(x):
     puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#',
               '*', '+', '\\', '•', '~', '@', '£',
@@ -22,3 +25,53 @@ def remove_names(x):
         if word[0] == "@":
             x = x.replace(word, "")
     return x
+
+
+def sep_digits(x):
+    return " ".join(re.split('(\d+)', x))
+
+
+def sep_punc(x):
+    punc = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~؛،؟؛.»«”'
+    out = []
+    for char in x:
+        if char in punc:
+            out.append(' '+char+' ')
+        else:
+            out.append(char)
+    return ''.join(out)
+
+damma = "ُ"
+sukun = "ْ"
+fatha = "َ"
+kasra = "ِ"
+shadda = "ّ"
+tanweendam = "ٌ"
+tanweenfath = "ً"
+tanweenkasr = "ٍ"
+tatweel = "ـ"
+
+tashkil = (damma, sukun, fatha, kasra, shadda, tanweendam, tanweenfath, tanweenkasr, tatweel)
+
+
+def remove_tashkil(word):
+    w = [letter for letter in word if letter not in tashkil]
+    return "".join(w)
+
+
+def clean_arabic(x):
+    return sep_punc(sep_digits(remove_tashkil(x)))
+
+
+def normalize(some_string):
+    normdict = {
+        'ة': 'ه',
+        'أ': 'ا',
+        'إ': 'ا',
+        'ي': 'ى',
+
+    }
+    out = [normdict.get(x, x) for x in some_string]
+    return ''.join(out)
+
+
